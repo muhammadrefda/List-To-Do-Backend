0cfd94b53e58caef0e78d2d4e54ebf471d73da2f
muhammad_refda <muhammad.refda@payment2go.co.id>
2024-08-21 00:06:55 +0700

    Adjust the migration

--- kangacook_test_backend/settings.py
+++ kangacook_test_backend/settings.py
@@ -30,17 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework_simplejwt',
-    'kangacook_test_backend'
-]
+
 
 
 MIDDLEWARE = [
@@ -148,3 +138,14 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'kangacook_test_backend'
+]
