406ab64c35e003199c3da918531ad1666f794887
muhammad_refda <muhammad.refda@payment2go.co.id>
2024-08-21 00:46:28 +0700

    work in progress

--- kangacook_test_backend/urls.py
+++ kangacook_test_backend/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/todos/', TodoListCreateView.as_view(), name='todo_list_create'),
-    path('api/todos/<int:pk>/', TodoDetailView.as_view(), name='todo_detail'),
+    path('api/todos/<int:pk>', TodoDetailView.as_view(), name='todo_detail'),
 ]
 
