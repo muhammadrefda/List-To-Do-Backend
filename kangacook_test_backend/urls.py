46d767300a6a73c0206a3c46fd19b0e02d94992d
muhammad_refda <muhammad.refda@payment2go.co.id>
2024-08-20 22:28:24 +0700

    Adjust Url based on project's need

--- kangacook_test_backend/urls.py
+++ kangacook_test_backend/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import TodoListCreateView, TodoDetailView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/todos/', TodoListCreateView.as_view(), name='todo_list_create'),
+    path('api/todos/<int:pk>/', TodoDetailView.as_view(), name='todo_detail'),
 ]
+
